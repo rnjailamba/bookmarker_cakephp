6a1b2053a1622252594e1e97c6d0fc9ead4cb25c
rnjai <rnjailamba12@gmail.com>
2015-04-26 12:40:56 +0530

    Login added

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -36,7 +36,24 @@ class AppController extends Controller
      */
     public function initialize()
     {
-        parent::initialize();
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'email',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ]
+        ]);
+
+        // Allow the display action so our pages controller
+        // continues to work.
+        $this->Auth->allow(['display']);
     }
 }
